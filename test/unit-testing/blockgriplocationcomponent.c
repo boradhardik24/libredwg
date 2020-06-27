94ab77ec9945f9f050bf350e195d71deb5b729ea
Reini Urban <rurban@cpan.org>
2020-06-27 10:32:49 +0200

    promote BLOCKGRIPLOCATIONCOMPONENT, BLOCKVISIBILITYGRIP
    to unstable

--- test/unit-testing/blockgriplocationcomponent.c
+++ test/unit-testing/blockgriplocationcomponent.c
@@ -1,4 +1,3 @@
-// TODO DEBUGGING
 #define DWG_TYPE DWG_TYPE_BLOCKGRIPLOCATIONCOMPONENT
 #include "common.c"
 
@@ -12,11 +11,9 @@ api_process (dwg_object *obj)
   BITCODE_T eval_expr; // one of: X Y UpdatedX UpdatedY DisplacementX DisplacementY
 
   Dwg_Version_Type dwg_version = obj->parent->header.version;
-#ifdef DEBUG_CLASSES
   dwg_obj_blockgriplocationcomponent *_obj = dwg_object_to_BLOCKGRIPLOCATIONCOMPONENT (obj);
 
   CHK_EVALEXPR (BLOCKVISIBILITYGRIP);
   CHK_ENTITY_TYPE (_obj, BLOCKVISIBILITYGRIP, grip_type, BL);
   CHK_ENTITY_UTF8TEXT (_obj, BLOCKVISIBILITYGRIP, eval_expr);
-#endif
 }
